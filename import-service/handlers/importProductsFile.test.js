767e8f468a740ce682a0e144652cb232f9700b91
Siarhei Nikitsin <siarhei_nikitsin@epam.com>
2022-07-31 14:49:37 +0400

    Task 5

--- import-service/handlers/importProductsFile.test.js
+++ import-service/handlers/importProductsFile.test.js
@@ -4,13 +4,18 @@ import { handler } from "./importProductsFile";
 
 test("returns correct signedUrl", async () => {
   const fileName = "file1.csv";
-  const expectedSignedUrl = "signed url";
+  const key = `uploaded/${fileName}`;
+  const generateSignedUrl = (key) => `signed url - ${key}`;
 
-  AWS.mock("S3", "getSignedUrl", () => expectedSignedUrl);
+  AWS.mock("S3", "getSignedUrl", (_method, params, callback) => {
+    const signedUrl = generateSignedUrl(params.Key);
+
+    callback(null, signedUrl);
+  });
 
   const { signedUrl } = await handler({
     queryStringParameters: { name: fileName },
   });
 
-  expect(signedUrl).toEqual(expectedSignedUrl);
+  expect(signedUrl).toEqual(generateSignedUrl(key));
 });
