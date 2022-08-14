5e7e0076377ed31bd6a75f9f74b231e71dbc092a
Siarhei Nikitsin <siarhei_nikitsin@epam.com>
2022-08-14 15:10:12 +0400

    Task 7

---
+++ authorization-service/handler.js
@@ -0,0 +1,3 @@
+import basicAuthorizer from './handlers/basicAuthorizer';
+
+export { basicAuthorizer };
---
+++ authorization-service/handlers/basicAuthorizer.js
@@ -0,0 +1,72 @@
+const generatePolicy = (principalId, resource, effect = "Allow") => ({
+  principalId,
+  policyDocument: {
+    Version: "2012-10-17",
+    Statement: [
+      {
+        Action: "execute-api:Invoke",
+        Effect: effect,
+        Resource: resource,
+      },
+    ],
+  },
+});
+
+const handler = async (event, _, cb) => {
+  console.log("Event: ", JSON.stringify(event));
+
+  const { type, authorizationToken, methodArn } = event;
+
+  if (type !== "TOKEN") {
+    cb("Unauthorized");
+  }
+
+  try {
+    const authorizationTokenParts = authorizationToken.split(" ");
+
+    let isAllowed = true;
+    let userName = "unknown";
+
+    if (
+      !authorizationToken ||
+      authorizationTokenParts.length !== 2 ||
+      authorizationTokenParts[0] !== "Basic" ||
+      !authorizationTokenParts[1]
+    ) {
+      isAllowed = false;
+    } else {
+      try {
+        const encodedCredentials = authorizationToken.split(" ")[1];
+
+        const buffer = Buffer.from(encodedCredentials, "base64");
+        const [login, password] = buffer.toString("utf-8").split(":");
+        
+        userName = login;
+
+        console.log(
+          `username: ${userName} and password: ${password}, buffer = ${buffer.toString(
+            "utf-8"
+          )}`
+        );
+
+        const expectedUserPassword = process.env[userName];
+
+        isAllowed = expectedUserPassword === password;
+      } catch (e) {
+        isAllowed = false;
+      }
+    }
+
+    const policy = generatePolicy(
+      userName,
+      methodArn,
+      isAllowed ? "Allow" : "Deny"
+    );
+
+    cb(null, policy);
+  } catch (error) {
+    cb(`Unauthorized: ${error.message}`);
+  }
+};
+
+export default handler;
